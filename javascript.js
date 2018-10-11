d76955b2d810c414e2454adb91b04a54c5d36bd5
povilasvit <povilasvitkauskass@gmail.com>
2018-10-11 18:30:53 +0300

    vk

---
+++ javascript.js
@@ -0,0 +1,19 @@
+var number = document.getElementById('number');
+
+function plus() {
+	console.log('Pridėti');
+	
+}
+
+
+function minus() {
+	console.log('Atimti');
+	
+}
+
+function generate () {
+	var number = document.getElementById('number');
+	for (var i = 100; i = getElementById ('number'); i--) {
+		Things[i]
+	}
+}
